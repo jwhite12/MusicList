45dbf688695104a2f1a7fdb719a86f8d8323f895
Joe White <jwhite8012@gmail.com>
2015-03-17 11:33:15 -0400

    Revert "almost done with 1"
    This reverts commit 87b8e008dfc69656b02890712a4bd356597d0fbe.

--- Song.java
+++ Song.java
@@ -1,18 +1,14 @@
 public class Song {
-public String artist;
-public String name;
-public int year;
-public double score;
-public String notes;
-// More fields here
-// Default constructor.. you'll need to modify this
-public Song(String artist, String name, int year, double score, String notes) {
-this.artist = artist;
-this.name = name;
-this.year = year;
-this.score = score;
-this.notes = notes;
-}
+  
+  public String artist;
+  public String name;
+  // More fields here
+  
+  // Default constructor.. you'll need to modify this
+  public Song(String artist, String name) { 
+    this.artist = artist;
+    this.name = name;
+  }
   
   /* ADD YOUR CODE HERE */
   
